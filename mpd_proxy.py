b29f1dd8a59b024065185390f7d66ea128bf0b33
Saurabh Sharan <saurabh.sharan1@gmail.com>
2015-02-04 21:34:44 -0800

    send OK message to client

--- mpd_proxy.py
+++ mpd_proxy.py
@@ -4,45 +4,64 @@ class MPDServerConn:
   def __init__(self, host, port):
     self.host = host
     self.port = port
+    self.conn = None
 
   def connect(self):
     self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.conn.connect((self.host, self.port))
 
-  def process_command(command_str):
+  def close(self):
+    if not self.conn:
+      return
+
+    self.conn.close()
+    self.conn = None
+
+  def process_command(self, command_str):
+    if not self.conn:
+      return
+
     self.conn.sendall(command_str)
 
-SERVERS = [('localhost', 8888)]
+SERVERS = [('localhost', 4007)]
 
 server_conns = []
 
+LISTEN_PORT = 3458
+
 def main():
-  for server in SERVERS:
-    s = MPDServerConn(server[0], server[1])
+  for server_host, server_port in SERVERS:
+    s = MPDServerConn(server_host, server_port)
     s.connect()
     server_conns.append(s)
 
-  client_listen_socket = socket.socket(socket.AF_INET, sock.SOCK_STREAM)
-  client_listen_socket.bind(('', 7777))
+  print 'Listening on port %d' % LISTEN_PORT
+
+  client_listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client_listen_socket.bind(('', LISTEN_PORT))
   client_listen_socket.listen(10)
 
   while True:
     print 'Waiting for client to connect to proxy...'
 
-    client_socket, client_address = client_listen_socket.accept()
+    try:
+      client_socket, client_address = client_listen_socket.accept()
 
-    client_data = ''
+      client_socket.sendall("OK MPD 0.19.8\n")
 
-    while True:
-      client_data += conn.recv()
-      if client_data.find('\n') != -1:
-        # process_command(client_data[:(client_data.find('\n') + 1)])
-        for server_conn in server_conns:
-          server_conn.process_command(client_data[:(client_data.find('\n') + 1)])
-      break
-        # client_data = client_data[(client_data.find('\n') + 1):]
+      client_data = ''
 
-    client_socket.close()
+      while True:
+        print "Waiting for data from client..."
+        client_data += client_socket.recv(1)
+
+        if client_data.find('\n') != -1:
+          for server_conn in server_conns:
+            server_conn.process_command(client_data[:(client_data.find('\n') + 1)])
+          break
+    finally:
+      print "Closing client socket after exception"
+      client_socket.close()
 
 if __name__ == '__main__':
   main()
